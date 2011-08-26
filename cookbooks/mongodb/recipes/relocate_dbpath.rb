616fe78dd7efe831afc815959b00e86e0ab07b7b
Chris Fordham <chris@xhost.com.au>
2011-08-26 18:03:27 +1000

    Remove includ_recipe.

--- cookbooks/mongodb/recipes/relocate_dbpath.rb
+++ cookbooks/mongodb/recipes/relocate_dbpath.rb
@@ -17,8 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe("mongodb::default")
-
 log "Relocate MongoDB dbpath."
 
 service "mongodb" do
